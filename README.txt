0872c45a82fffc26966306b3264bf503afa640a4
RobertOfTaiwan <RobertOfTaiwan@gmail.com>
2014-04-27 11:20:05 +0800

    First Commit

--- README.txt
+++ README.txt
@@ -1,3 +1,10 @@
 Sudoku Solving Library
 ----------------------
 
+For the purpose of studying solving Sudoku and learning python programming,
+including traditional method and oop.
+
+1. Traditional method: matrix
+2. oop method: sudoku
+
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = []
+
+setup(name='SudokuStudyLib',
+      version='0.5',
+      description='Sudoku Solving Library',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Games/Entertainment :: Puzzle Games",
+          "Topic :: Education",
+      ],
+      author='Robert J. Hwang',
+      author_email='RobertOfTaiwan@gmail.com',
+      keywords='sudoku, python, studying',
+      packages=['sudoku', 'matrix'],
+      license='MIT',
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+)
+
